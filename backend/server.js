633fb9d6d9ca6e93551c194107706bf05cf6abf3
kenpin9902 <kenpin9902@gmail.com>
2025-06-09 05:14:17 +0800

    Connect To MongoDB

---
+++ backend/server.js
@@ -0,0 +1,12 @@
+import express from "express";
+import { connectDB } from "./config/db.js";
+import dotenv from "dotenv";
+dotenv.config();
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+connectDB();
+
+app.listen(PORT, () =>console.log(`Server running on port ${PORT}`));
